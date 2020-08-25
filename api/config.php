1f9c355d7df1dfb684bba4e01c9f0ffa0913c26e
ginwa123 <gilangtrisetya123@gmail.com>
2020-08-25 19:51:58 +0700

    add function message

--- api/config.php
+++ api/config.php
@@ -1,14 +1,38 @@
 <?php
 
-define('HOSTNAME','localhost');
+define('HOSTNAME', 'localhost');
 
-define('USERNAME','root');
+define('USERNAME', 'root');
 
-define('PASSWORD','');
+define('PASSWORD', '');
 
-define('DB_NAME','db_duj');
+define('DB_NAME', 'db_duj');
 
 
 
-$koneksi = new mysqli(HOSTNAME,USERNAME,PASSWORD,DB_NAME);
-?>
+$koneksi = new mysqli(HOSTNAME, USERNAME, PASSWORD, DB_NAME);
+
+function error_message($message = null)
+{
+    $preMessage = 'Error: ';
+    $response = array();
+    global $koneksi;
+    $response['code'] = 0;
+    if ($message == null) {
+        $response['message'] =  $preMessage . mysqli_error($koneksi);
+        return $response;
+    }
+    $response['message'] =  $preMessage . $message;
+    return $response;
+}
+
+function success_message($message = null)
+{
+
+    $preMessage = 'Success: ';
+    $response = array();
+    global $koneksi;
+    $response['code'] = 1;
+    $response['message'] = $preMessage . $message;
+    return $response;;
+}
