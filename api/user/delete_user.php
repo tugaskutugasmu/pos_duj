47ff539d3a36a63885e33d00411ece44c9915959
ginwa123 <gilangtrisetya123@gmail.com>
2020-08-25 19:49:30 +0700

    update message, clean code

--- api/user/delete_user.php
+++ api/user/delete_user.php
@@ -1,49 +1,20 @@
 <?php include_once('../config.php');
 
 $id_user = addslashes(htmlentities($_POST['id_user']));
-
 $getdata = mysqli_query($koneksi, "SELECT * FROM user WHERE id_user = '$id_user'");
 $rows = mysqli_num_rows($getdata);
 
 if ($rows > 0) {
   $query = "DELETE FROM user WHERE id_user = '$id_user'";
   $delete = mysqli_query($koneksi, $query);
-  $response = array();
+  $response = null;
   if ($delete) {
-    $response['code'] = 1;
-    $response['message'] = "Deleted Success";
+    $response = success_message("Deleted : . $id_user");
   } else {
-    $response['code'] = 0;
-    $response['message'] = mysqli_error($koneksi);
+    $response = error_message();
   }
 } else {
-  $response['code'] = 0;
-  $response['message'] = "data not found";
+  $response = error_message("Data not found");
 }
 
 echo json_encode($response);
-
-// $id = addslashes(htmlentities($_POST['id']));
-// $getdata = mysqli_query($koneksi,"SELECT * FROM userr WHERE id = '$id'");
-// $rows = mysqli_num_rows($getdata);
-
-// $delete = "DELETE FROM userr WHERE id = '$id'";
-// $exedelete = mysqli_query($koneksi,$delete);
-
-// $respose = array();
-// if($rows > 0)
-// {
-//   if ($exedelete) {
-//     $respose['code'] = 1;
-//     $respose['message'] = "Deleted Success";
-//   }else{
-//     $respose['code'] = 0;
-//     $respose['message'] = "Failed to Delete";
-//   }
-// }else{
-//   $respose['code'] = 0;
-//   $respose['message'] = "Failed to Delete, data Not Found";
-// }
-
-
-// echo json_encode($respose);
